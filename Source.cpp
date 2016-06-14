060a861b8865e28f389ac474327a9ff8bcbc5a3a
kenji <kenji256@gmail.com>
2016-06-14 23:41:07 +0900

    add files.

---
+++ Source.cpp
@@ -0,0 +1,82 @@
+﻿#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+#include <windows.h>
+#include <atlbase.h>
+#include <atlctl.h>
+#include "resource.h"
+
+CComModule _Module;
+
+BEGIN_OBJECT_MAP(ObjectMap)
+END_OBJECT_MAP()
+
+TCHAR szClassName[] = TEXT("Window");
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	static HWND	hBrowser;
+	switch (msg)
+	{
+	case WM_CREATE:
+	{
+		TCHAR szModulePath[MAX_PATH];
+		GetModuleFileName(0, szModulePath, MAX_PATH);
+		TCHAR szNavigateToResource[1024];
+		wsprintf(szNavigateToResource, L"res://%s/%d", szModulePath, IDR_HTML1);
+		hBrowser = CreateWindow(TEXT("AtlAxWin140"), szNavigateToResource, WS_CHILD | WS_VISIBLE | WS_DISABLED, 0, 0, 0, 0, hWnd, 0, ((LPCREATESTRUCT)lParam)->hInstance, 0);
+	}
+	return 0;
+	case WM_SIZE:
+		MoveWindow(hBrowser, 0, 0, LOWORD(lParam), HIWORD(lParam), 1);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPSTR pCmdLine, int nCmdShow)
+{
+	AtlAxWinInit();
+	_Module.Init(ObjectMap, hInstance);
+	MSG msg;
+	WNDCLASS wndclass = {
+		0,
+		WndProc,
+		0,
+		0,
+		hInstance,
+		0,
+		LoadCursor(0, IDC_ARROW),
+		0,
+		0,
+		szClassName
+	};
+	RegisterClass(&wndclass);
+	HWND hWnd = CreateWindow(
+		szClassName,
+		TEXT("SVG画像表示"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		0,
+		0,
+		hInstance,
+		0
+	);
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while (GetMessage(&msg, 0, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	_Module.Term();
+	AtlAxWinTerm();
+	return (int)msg.wParam;
+}
